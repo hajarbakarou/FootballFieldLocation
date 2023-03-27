4a6d7281ba2e109bbfbfb962442201e126b5a467
hajarbakarou <bakarouhajar1@gmail.com>
2023-03-27 10:41:55 +0000

    first

---
+++ src/main/java/pfa/app/FootballFieldLocationApplication.java
@@ -0,0 +1,13 @@
+package pfa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFieldLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballFieldLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pfa/app/FootballFieldLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package pfa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballFieldLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
